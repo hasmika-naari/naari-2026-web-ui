576323af05deb8151b6ee47e21704f3bbe6ea74d
hasmika-naari <hasmika.naari@gmail.com>
2025-11-19 13:15:29 -0500

    dashboard updates

--- src/app/common/sidebar/sidebar.component.ts
+++ src/app/common/sidebar/sidebar.component.ts
@@ -8,9 +8,10 @@ import { FeathericonsModule } from '../../icons/feathericons/feathericons.module
 
 @Component({
     selector: 'app-sidebar',
-    imports: [NgScrollbarModule, MatExpansionModule, RouterLinkActive, RouterModule, RouterLink, NgClass, FeathericonsModule],
-    templateUrl: './sidebar.component.html',
-    styleUrl: './sidebar.component.scss'
+    imports: [NgScrollbarModule, MatExpansionModule, 
+        RouterLinkActive, RouterModule, RouterLink, NgClass, FeathericonsModule],
+    templateUrl: './sidebar.component.html',    
+    styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent {
 
